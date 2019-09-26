45a26c971d105412f5a6ff908ac04355d65d0e7f
maxim <maximmahnev@gmail.com>
2019-09-27 03:09:03 +0700

    erbdrtr6n

---
+++ UsefulThings/Func/Helpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+namespace UsefulThings.Func
+{
+    public class Helpers
+    {
+        public static StringCollection GetStringCollection(string list)
+        {
+            StringCollection coll = new StringCollection();
+
+            string[] s = list.Split(new Char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string str in s)
+            {
+
+                char[] charsToTrim = { '\n', '\r' };
+                string str_trim = str.Trim(charsToTrim);
+                if (str_trim != null && str_trim != "" && str_trim != String.Empty)
+                {
+                    coll.Add(str_trim);
+                }
+            }
+            return coll;
+        }
+    }
+}
